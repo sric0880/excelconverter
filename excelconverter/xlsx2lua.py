b4c903e526a85970e026ad9761220e49a5403ee1
sric <lzqiong@gmail.com>
2017-03-28 00:09:18 +0800

    Fix: objectlist type xlsx convert to key value pairs

--- excelconverter/xlsx2lua.py
+++ excelconverter/xlsx2lua.py
@@ -1,8 +1,12 @@
 # encoding: utf-8
 from slpp import slpp as lua
-from .xlsx2x import parseObject
+from xlsx2x import parseObject
 
 def convert(xlsxfile, tablename):
+	o = parseObject(xlsxfile)
+	if isinstance(o, list) and type(o[0]) is dict:
+		if 'Key' in o[0] and 'Value' in o[0]:
+			o = dict([ (item['Key'], item['Value']) for item in o ])
 	tempLuaFilename = xlsxfile.replace('.xlsx', '.lua')
 	with open(tempLuaFilename, 'w') as tempLuaFile:
-		tempLuaFile.write(tablename + ' = ' + lua.encode(parseObject(xlsxfile)))
+		tempLuaFile.write(tablename + ' = ' + lua.encode(o))
